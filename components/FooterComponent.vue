5865b71c0790b85fd3a23c6c6cc1668bdd4c8052
Mustam <mustamlimbu@gmail.com>
2023-12-03 15:17:40 +0545

    first commit

---
+++ components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>I am Footer</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        
+        <h1>hahaha</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
